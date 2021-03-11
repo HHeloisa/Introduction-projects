bba4ad3fc8d3ea850d87741303a322d650e2ed74
hheloisa <hhackenhaar@gmail.com>
2021-03-11 19:17:18 -0300

    Requisito 5 - agora sim!

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
+// Inserindo o botão em uma variável
+const buutoom = document.getElementById('criar-tarefa');
+// Adicionando o eventListener ao botão
+buutoom.addEventListener('click', addTask);
+// Cria variavel que cria nova linha
+
+
 function addTask() {
-  const buutoom = document.getElementById('criar-tarefa');
-  buutoom.addEventListener('click', addTask);
   const newTask = document.createElement('li');
+  // captura o conteudo do imput
   const inputContent = document.getElementById('texto-tarefa').value;
+  // adiciona a nova linha o conteudo do imput
   newTask.textContent = inputContent;
-  newTask.className = 'task';
-  document.getElementById('lista-tarefas').appendChild(newTask);
+  // adiciona classe a nova linha
+  newTask.classList.add = 'task';
+  // adiciona a ol a nova linha
+  document.querySelector('#lista-tarefas').appendChild(newTask);
+  // limpa o imput
   document.getElementById('texto-tarefa').value = '';
 }
-addTask();
+
+/* // Requisito 7
+function selectedTask() {
+  const listeningToOl = document.getElementById('lista-tarefas');
+  listeningToOl.addEventListener('click', (e => {
+    const listeningToLi = e.target;
+    const selectedElement = document.getElementsByClassName('selected');
+
+    listeningToLi.classList = 'selected';
+    listeningToLi.style.backgroundColor = 'rgb(128, 128, 128)'
+  }));
+}
+selectedTask(); */
+
+/* const colorElement = e.target;
+const selectedElement = document.querySelector('.selected');
+selectedElement.classList.remove('selected');
+colorElement.classList.add('selected');
+});
+} */
