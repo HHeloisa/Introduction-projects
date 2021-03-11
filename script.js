26c6c0b189e934545633ae23d8ac9560a74fbcb4
hheloisa <hhackenhaar@gmail.com>
2021-03-11 17:45:33 -0300

    Requisito 5: limpa imput apos envio

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function addTask() {
   const newTask = document.createElement('li');
   const inputContent = document.getElementById('texto-tarefa').value;
   newTask.textContent = inputContent;
+  newTask.className = 'task';
   document.getElementById('lista-tarefas').appendChild(newTask);
+  document.getElementById('texto-tarefa').value = '';
 }
 addTask();
