913ed41ff9651c8d73d26caac672122ea853dc98
hheloisa <hhackenhaar@gmail.com>
2021-03-29 16:33:19 -0300

    Rereformula requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,12 +17,12 @@
 */
 // Referência: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Template_literals;
 const vqv = (nome, idade) => {
-  if (nome !== undefined && idade !== undefined) {
-    return `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
-  }
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  } return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
