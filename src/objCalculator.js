c384f6f2208fe3efa95c71c6eaf59132ffdd4b1d
hheloisa <hhackenhaar@gmail.com>
2021-03-29 17:58:31 -0300

    Inicia requisito 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,18 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+// Referência: https://www.w3schools.com/jsref/jsref_floor.asp;
+
+const add = (num1, num2) => Math.floor(num1 + num2);
+const mult = (num1, num2) => Math.floor(num1 * num2);
+const div = (num1, num2) => Math.floor(num1 / num2);
+const sub = (num1, num2) => Math.floor(num1 - num2);
+
+const calculator = {
+  add,
+  mult,
+  div,
+  sub,
+};
 
 module.exports = calculator;
