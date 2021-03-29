fa12e33707cb17b3d380042c0780373c94b7a812
hheloisa <hhackenhaar@gmail.com>
2021-03-29 18:38:13 -0300

    Insere requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -11,14 +11,14 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  const myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let otherCounter = 2; otherCounter <= 3; otherCounter += 1) {
+      myArray.push(otherCounter);
     }
   }
   return myArray;
 };
-
+console.log(myCounter());
 module.exports = myCounter;
