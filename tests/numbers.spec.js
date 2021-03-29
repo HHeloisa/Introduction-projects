02b6e8377bd215f0381d6ad26986c810be5464de
hheloisa <hhackenhaar@gmail.com>
2021-03-29 20:02:31 -0300

    Corrige requisito 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -19,12 +19,12 @@ const numbers = require('../src/numbers');
 describe('#numbers', () => {
   it('should return an array and return if it has only numbers or not', () => {
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
-    assert.strictEqual([1, 2, 3, 4, 5], true);
+    assert.strictEqual(numbers([1, 2, 3, 4, 5]), true);
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
-    assert.strictEqual([1, 2, '3', 4, 5], false, 'retornou false');
+    assert.strictEqual(numbers([1, 2, '3', 4, 5]), false, 'retornou false');
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
-    assert.strictEqual([1, 'a', 3], false);
+    assert.strictEqual(numbers([1, 'a', 3]), false);
     // Escreva um teste em que a função recebe [' '] e retorna false
-    assert.strictEqual([' '], false);
+    assert.strictEqual(numbers([' ']), false);
   });
 });
