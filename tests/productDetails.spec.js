11f8bd9126e2a5262667686c7742fc54940576b1
hheloisa <hhackenhaar@gmail.com>
2021-03-30 12:34:33 -0300

    Corrige erro do lint

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -41,7 +41,8 @@ describe('#productDetails', () => {
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(productDetails('bolacha', 'pão').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    assert.strictEqual(typeof productDetails('bolacha', 'pão')[0] && productDetails('bolacha', 'pão')[1], 'object');
+    assert.strictEqual(typeof productDetails('bolacha', 'pão')[0]
+    && productDetails('bolacha', 'pão')[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
     assert.notStrictEqual(productDetails('bolacha', 'pão')[0], productDetails('bolacha', 'pão')[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
