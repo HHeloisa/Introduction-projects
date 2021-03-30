4bf303034773d3f46d7c47469882c08f9a4d1904
hheloisa <hhackenhaar@gmail.com>
2021-03-30 13:51:41 -0300

    Remove comentarios

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -49,24 +49,3 @@ describe('#productDetails', () => {
     assert.strictEqual(productDetails('bolacha', 'pão')[1].details.productId, `${'pão'}123`);
   });
 });
-
-/*
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna um objeto.
-    assert.strictEqual(typeof circle(1), 'object');
-    // Teste se o objeto retornado tem 3 entradas.
-    assert.strictEqual(Object.keys(circle(1)).length, 3);
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    assert.strictEqual(circle(), undefined);
-    // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    assert.strictEqual(circle(2).circumference, 12.56);
-    // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.strictEqual(parseFloat((circle(3).area).toFixed(2)), 28.26);
-    // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    const testCircle = circle(3);
-    testCircle.area = parseFloat((testCircle.area).toFixed(2));
-    testCircle.circumference = parseFloat((testCircle.circumference).toFixed(2));
-    assert.deepStrictEqual(testCircle, { radius: 3, area: 28.26, circumference: 18.84 });
-  });
-});
- */
