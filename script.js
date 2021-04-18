edbe4db42932d4b3002d7f924e9c664fa77e1466
hheloisa <hhackenhaar@gmail.com>
2021-04-18 16:28:35 -0300

    Insere requisito 2 sem erro do lint

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
 
-  //li.addEventListener('click', cartItemClickListener);
+  // li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
