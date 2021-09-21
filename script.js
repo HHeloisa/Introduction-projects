3047d535e57da36d7b871f42ffded38543ec976c
hheloisa <hhackenhaar@gmail.com>
2021-09-20 23:00:18 -0300

    Refatora e cria novas funcionalidades: Adicionar cores e definir tamanho do quadro

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-let size = 5;
+let size = 10;
+
+const paletaCores = 'color-palette';
 // Requisito 2- Criando paleta de cores
 function creatingColorPaddles() {
-  for (let index = 0; index < 4; index += 1) {
+  for (let index = 0; index < 10; index += 1) {
     const creatColorPaddles = document.createElement('div');
-    document.getElementById('color-palette').appendChild(creatColorPaddles)
+    document.getElementById(paletaCores).appendChild(creatColorPaddles)
       .style.listStyle = 'none';
     creatColorPaddles.classList = 'color';
   }
@@ -16,7 +18,13 @@ function paytingColorPaddles() {
   colorsPaddles[0].style.backgroundColor = 'black';
   colorsPaddles[1].style.backgroundColor = 'purple';
   colorsPaddles[2].style.backgroundColor = 'gray';
-  colorsPaddles[3].style.backgroundColor = 'yellow';
+  colorsPaddles[3].style.backgroundColor = 'white';
+  colorsPaddles[4].style.backgroundColor = 'red';
+  colorsPaddles[5].style.backgroundColor = '#f72585';
+  colorsPaddles[6].style.backgroundColor = '#f9c74f';
+  colorsPaddles[7].style.backgroundColor = '#fec5bb';
+  colorsPaddles[8].style.backgroundColor = '#0096c7';
+  colorsPaddles[9].style.backgroundColor = '#2a9d8f';
 }
 paytingColorPaddles();
 
@@ -47,7 +55,7 @@ blackColor();
 // Requisito 7 - Realizado em conjunto com colegas, um grupo de ajuda mutua. Aprendemos juntos, em especial, ao Murilo Gonçalves que explicou a utilização do (e).
 function classChange() {
   const elementColors = document.getElementById('color-palette');
-  elementColors.addEventListener('click', function (e) {
+  elementColors.addEventListener('click', (e) => {
     const colorElement = e.target;
     const selectedElement = document.querySelector('.selected');
     selectedElement.classList.remove('selected');
@@ -59,7 +67,7 @@ classChange();
 // Requisito 8 - Realizado em conjunto com colegas, um grupo de ajuda mutua. Aprendemos juntos, em especial, ao Murilo Gonçalves que explicou a utilização do (e).
 function paintPixel() {
   const paintBoard = document.querySelector('#pixel-board');
-  paintBoard.addEventListener('click', function (e) {
+  paintBoard.addEventListener('click', (e) => {
     const pixel = e.target;
     if (pixel.className === 'pixel') {
       const colorSelected = document.querySelector('.selected').style.backgroundColor;
@@ -70,9 +78,6 @@ function paintPixel() {
 paintPixel();
 
 // Requisito 9 - Criar botão para limpar quadro
-const buttomClean = document.querySelector('#clear-board');
-buttomClean.addEventListener('click', clearBoard);
-
 function clearBoard() {
   const pixel = document.querySelectorAll('.pixel');
   for (let index = 0; index < pixel.length; index += 1) {
@@ -80,3 +85,32 @@ function clearBoard() {
   }
 }
 clearBoard();
+
+const buttomClean = document.querySelector('#clear-board');
+buttomClean.addEventListener('click', clearBoard);
+
+function changeInputNumber() {
+  const inputNumber = document.getElementById('inputNumber');
+  const inputValue = inputNumber.value;
+  size = inputValue;
+  document.querySelector('#pixel-board').remove();
+  creatingPixelPaintingBoard();
+}
+
+const buttonInputNumber = document.querySelector('#buttonInputNumber');
+buttonInputNumber.addEventListener('click', changeInputNumber);
+
+function createNewColor() {
+  const InputNewColor = document.querySelector('#inputHexaColor');
+  const inputValue = InputNewColor.value;
+  console.log(inputValue);
+
+  const newColor = document.createElement('div');
+  document.getElementById(paletaCores).appendChild(newColor)
+    .style.listStyle = 'none';
+  newColor.classList = 'color';
+  newColor.style.backgroundColor = `#${inputValue}`;
+}
+
+const buttonNewColor = document.querySelector('#buttonNewColor');
+buttonNewColor.addEventListener('click', createNewColor);
